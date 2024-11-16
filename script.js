e4d0964f367fde096c3e910ee4bfa1257730a8b3
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-11-16 13:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+let table = document.getElementById("myTable");
+
 function createTable() {
-    //Write your code here
+  //Write your code here
+  let rows = Number(prompt("Input number of rows"));
+  let cols = Number(prompt("Input number of columns"));
   
+
+  for (let i = 0; i < rows; i++) {
+    let tableRows = document.createElement("tr");
+
+    for (let j = 0; j < cols; j++) {
+      let tableData = document.createElement("td");
+      tableData.innerHTML = `Row-${i} Column-${j}`
+      tableRows.appendChild(tableData);
+    }
+    table.appendChild(tableRows);
+  }
 }
